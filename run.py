9ca9d49d3932bb7822cac4198ab6a76d40dff7b1
HaxTeR-Hacker <114458983+HaxTeR-Hacker@users.noreply.github.com>
2022-09-27 11:26:38 +0700

    V0

---
+++ run.py
@@ -0,0 +1,9 @@
+# FREE VERSION, BUY PREMIUM AT https://discord.gg/NqeSeDaVYj THX
+# DONT COPY THE CODE, CREDIT PLZ! MADE BY HaxTeR!
+from colorama import Fore
+import time
+print(Fore.RED + "STARTING FREE VERSION...")
+print(Fore.GREEN + "Buy premium at https://discord.gg/NqeSeDaVYj")
+time.sleep(1)
+webbrowser.open('https://discord.gg/NqeSeDaVYj')
+time.sleep("0.1")
